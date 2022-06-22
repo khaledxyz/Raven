c1d91abc2ff99f838583d5f01c5d0daf2e3ec15e
attano001 <attano001@outlook.com>
2022-06-22 12:02:33 +0100

    Created Goal Model w/ basic CRUD functions.

--- server/src/controllers/golasController.js
+++ server/src/controllers/golasController.js
@@ -1,35 +1,64 @@
 const asyncHandler = require('express-async-handler');
 
+const goalModel = require('../models/goalModel');
+
 // @desc    Get Goal
 // @route   GET /api/goals
 // @acess   Private
 const getGoals = asyncHandler(async(req, res) => {
-    res.status(200).json({message: 'Get Goals'})
+    const goals = await goalModel.find();
+    res.status(200).json({goals})
 });
 
 // @desc    set Goal
 // @route   POST /api/goals
 // @acess   Private
 const setGoal = asyncHandler(async(req, res) => {
-    if(!req.body.text) {
+    if(!req.body) {
         res.status(400);
         throw new Error('Empty Fields')
         return;
     }
-    res.status(200).json({message: 'Set Goal'})
+    const goal = await goalModel.create({
+        title: req.body.title
+    });
+
+    res.status(200).json({goal})
 });
 
 // @desc    update Goal
 // @route   PUT /api/goals/:id
 // @acess   Private
 const updateGoal = asyncHandler(async(req, res) => {
-    res.status(200).json({message: `Set Goal ${req.params.id}`})
+    const goal = await goalModel.findById(req.params.id);
+
+    if(!goal) {
+        res.status(404);
+        throw new Error('Goal not found!');
+        return;
+    }
+
+    const updatedGoal = await goalModel.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+    })
+
+    res.status(200).json(updatedGoal)
 });
 
 // @desc    delete Goal
 // @route   DELETE /api/goals/:id
 // @acess   Private
 const deleteGoal = asyncHandler(async(req, res) => {
+    const goal = await goalModel.findByIdAndDelete(req.params.id);
+
+    if(!goal) {
+        res.status(404);
+        throw new Error('Goal not found!');
+        return;
+    }
+
+    await goal.remove();
+
     res.status(200).json({message: `Delete Goal ${req.params.id}`})
 });
 
---
+++ server/src/models/goalModel.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const goalSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, 'Please, add a title.']
+    }
+},
+{timestamps: true});
+
+module.exports = mongoose.model('Goal', goalSchema);
