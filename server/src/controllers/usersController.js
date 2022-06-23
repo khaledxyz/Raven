1554a5e6b7ed393ee6274801b1f0736c748fcd43
attano001 <attano001@outlook.com>
2022-06-23 13:32:00 +0100

    POST user to DB functionality

--- server/src/controllers/usersController.js
+++ server/src/controllers/usersController.js
@@ -1,13 +1,38 @@
 const asyncHandler = require('express-async-handler');
 const userModel = require('../models/userModel');
-
+const bcrypt = require('bcryptjs');
 
 // @desc    Register User
 // @route   POST /api/users
 // @acess   Public
-const registerUser = (req, res) => {
-    res.status(200).json({message: 'Register User'});
-};
+const registerUser = asyncHandler(async(req, res) => {
+    const { username, email, password } = req.body;
+    console.log(req.body)
+
+    // Check if user exists
+    const userExists = userModel.findOne({email});
+    if(!userExists) {
+        res.status(409);
+        throw new Error('Account already exists.');
+        return;
+    };
+    
+    // Hash password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+    
+    // Push user to the DB
+    const user = await userModel.create({
+        username,
+        email,
+        password: hashedPassword
+    });
+
+    if(user) return res.status(201).json({user});
+
+    res.status(400)
+    throw new Error('Something went wrong. Try again.')
+});
 
 // @desc    Login User
 // @route   POST /api/users/login
--- server/src/models/userModel.js
+++ server/src/models/userModel.js
@@ -16,4 +16,4 @@ const userSchema = mongoose.Schema({
     },
 },{timestamps: true});
 
-module.exports = mongoose.model('userModel', userSchema);
+module.exports = mongoose.model('User', userSchema);
