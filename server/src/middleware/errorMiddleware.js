65d2abd86c7fa0b39f349ba92b02a760a44f1217
attano001 <attano001@outlook.com>
2022-07-05 22:26:05 +0100

    Minor spelling mistake.

--- server/src/middleware/errorMiddleware.js
+++ server/src/middleware/errorMiddleware.js
@@ -1,7 +1,7 @@
 const errorHandler = (error, req, res, next) => {
     const statusCode = res.statusCode ? res.statusCode : 500;
     res.statusCode(statusCode);
-    res.jsoon({
+    res.json({
         message: error.message,
         stack : process.env.NODE_ENV === 'development' ? error.stack : null
     });
